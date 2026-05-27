34360150814820b188a5258114e441fa0b6c3a0d
Zoia Krasnickia <zoya.ivanchuk@chat2desk.com>
2026-05-28 00:24:08 +0300

    ebany git

--- loops_practice.py
+++ loops_practice.py
@@ -11,5 +11,5 @@ class Practice:
                 break
             print(n)
 
-object_for_pract = Practice()
-object_for_pract.list_numbers()
+object_for_practice = Practice()
+object_for_practice.list_numbers()
